0983345708171097514920a319a3dc7fd5854e83
Adeoye Tobi <88192274+OluSure@users.noreply.github.com>
2022-08-25 17:09:47 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html lang="en" dir="ltr"> 	
+<head> 		
+<meta charset="utf-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 		
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 		
+<title>SEARCH BOARD</title> 	</head> 	<body> 		
+<div class="container"> 		
+<header> 			<h1>SEARCH BOARD</h1> 		</header>
+</div>
+<div class="nav"><div class="nav-link><a href="createdata.php" title="open this link once">Create Data</a>
+<?php include 'search.php'; ?>
+<br/><br/>
+<div class="md-1 footer-bottom copyright">
+<p>&#127912; with &#128150; by <a href="https://tobiadeoye.ga">Tobi Adeoye</a>  &copy; 2022 </p>
+				<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+				crossorigin="anonymous"></script>
+</body>
+</html>
